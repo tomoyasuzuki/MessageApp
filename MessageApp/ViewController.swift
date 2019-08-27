4cd7ec9cf7094d622aa3327dd45c69977a7dfdad
tomoyasuzuki <tomoya.s0121@icloud.com>
2019-08-28 00:55:24 +0900

    Initial Commit

---
+++ MessageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessageApp
+//
+//  Created by 鈴木友也 on 2019/08/28.
+//  Copyright © 2019 tomoya.suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
